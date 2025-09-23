bb04b29f5fee507181f11be4cf4b6fc08699c3d6
Alizée GUEYE <AG@def-systemes.local>
2025-09-23 11:51:16 +0200

    Changement environnement et package.json

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' // Backend local
+  apiUrl: 'http://localhost:8080/api' // backend local Spring Boot
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' // Backend Spring Boot en local
+  apiUrl: '/api' // utilisera proxy.conf.json en local
 };
